c893eda38990aa72826be9366dab78c79ea4d2dc
mohamed-elkadiri <mohamedelkadiri.info@gmail.com>
2021-05-12 17:03:41 +0200

    s4

--- resources/views/t1/panier-form.blade.php
+++ resources/views/t1/panier-form.blade.php
@@ -9,10 +9,31 @@
                     <p>2 Pièces</p>
                     <p>Montant global  : 35 €</p>
                 </div>
-                <div class=" d-flex  bg-form">
+                <div class="   bg-form">
                     <div class="row panier-form-body">
                         <div class="col-9 ">
-                            <div class="row">
+                            <div class="row  ">
+                                <div class="col-sm-3">
+                                    <img src="/assets/images/image 7.png" alt="" width="100px" height="100px">
+                                </div>
+                                <div class="col-sm-9 list-form">
+                                    <h3>Pavé de saumon fumé et citrons confits</h3>
+                                    <ul>
+                                        <li>BACON CHEESYERIES JALAPENOS</li>
+                                        <li>Menu XL</li>
+                                        <li>Sans Sauce</li>
+                                        <li>EAU PETILLANTE</li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                            <hr style="margin-top:30px;">
+                        </div>
+                        <div class="col-sm-3"><p>15.00€</p></div>
+                    </div>
+                    <div class="row panier-form-body">
+                        <div class="col-9 ">
+                            <div class="row  ">
                                 <div class="col-sm-3">
                                     <img src="/assets/images/image 7.png" alt="" width="100px" height="100px">
                                 </div>
@@ -32,8 +53,34 @@
                     </div>
 
                 </div>
+                <button class="panier-form-button" >Modifier</button>
+
             </div>
-            <div class="col-sm-6"></div>
+            <div class="col-sm-6">
+                <form action="">
+                    <div class="container">
+                        <div class="personnel-info">
+                            <h3>Informations personelles</h3>
+                            <input class="half-i" type="text"><input class="half-i" type="text">
+                            <input class="full-i" type="text">
+                            <div class="select">
+                                <select>
+                                    <option>--Select--</option>
+                                    <option>Hello 1</option>
+                                    <option>Hello 2</option>
+                                    <option>Hello 3</option>
+                                    <option>Hello 4</option>
+                                </select>
+                                <div class="select_arrow">
+                                </div>
+                            </div>
+                            <input class="half-i" type="text">
+                            <input class="half-i" type="text">
+
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
 </div>
